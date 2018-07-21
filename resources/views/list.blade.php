c05c96c87a09949c6b637781a14c07a81d6e662e
karthik <srkarthikg@gmail.com>
2018-07-21 17:28:27 +0530

    c

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -76,7 +76,7 @@
             <div class="col-lg-offset-3 col-lg-6">
 				<div class="panel panel-default">
 				  <div class="panel-heading">
-				    <h3 class="panel-success">Laravel - ToDo List (Click on Each Item to Update or Delete)<a href="#" class="pull-right" data-toggle="modal" data-target="#myModal" id="addNew"><i class="fa fa-plus" aria-hidden="true"></i></a></h3>
+				    <h3 class="panel-success">ToDo List<a href="#" class="pull-right" data-toggle="modal" data-target="#myModal" id="addNew"><i class="fa fa-plus" aria-hidden="true"></i></a></h3>
 				  </div>
 				  <div class="panel-body" id="items">
 				    @if(count($items)>0)
@@ -89,36 +89,31 @@
                             @endforeach
                         </ul>
                     @else
-                        <p>No item found !</p>
+                        <p>Todo list is empty please add some tasks.</p>
                     @endif  
-                    Total Items : <?php echo count($items);?><br/>
+                    No of Items : <?php echo count($items);?><br/>
                     Completed Items : <?php echo $completed;?><br/>
                     Pending Items : <?php echo $pending;?>
 				  </div>
 				</div>
 			</div>
-
-            <div class="col-lg-2">
-                <input type="text" name="searchItem" id="searchItem" class="form-control" placeholder="Search">
-            </div>
-
             <!-- modal -->
             <div class="modal fade" tabindex="-1" role="dialog" id="myModal">
                 <div class="modal-dialog modal-sm" role="document"><!-- modal-dialog -->
                     <div class="modal-content"><!-- modal-content -->
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="title">Create new item</h4>
+                            <h2 class="modal-title" id="title">Create new item list.</h2>
                         </div>
                         <div class="modal-body">
                             <input type="hidden" id="id">
                             <p><input type="text" id="addItem" placeholder="Please enter todo tasks" class="form-control"></p>
-                            <p>Please check the todo if tasks completed.<input type="checkbox" id="setItem" class="form-control"></p>
+                            <p>Please check the todo if tasks completed.<input type="checkbox" id="setItem" class="form-control collapse"></p>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-Danger" data-dismiss="modal" style="display:none;" id="delete">Delete</button>
                             <button type="button" class="btn btn-default" style="display:none;" data-dismiss="modal" id="update">Update</button>
-                            <button type="button" class="btn btn-info" data-dismiss="modal" id="add">Add item</button>
+                            <button type="button" class="btn btn-info" data-dismiss="modal" id="add">Add</button>
                         </div>
                     </div><!-- /.modal-content -->
                 </div><!-- /.modal-dialog -->
